0e16ff631cc4b21c81489f5dfd289938c4a29a8a
edhawx <deadlifecorps@gmail.com>
2024-12-10 16:06:54 +0000

    changing path again

--- front-end/foxhole-map-tool/vite.config.ts
+++ front-end/foxhole-map-tool/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/foxhole-tool/", // Match the homepage URL
+  base: "/foxhole-tool/front-end/foxhole-map-tool/", // Match the `homepage` path
   plugins: [react()],
 });
